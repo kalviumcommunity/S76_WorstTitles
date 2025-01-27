e7c1724ad2531f613af452140fa11f477c66efdd
suba dharshini <subakarthick2005@gmail.com>
2025-01-27 11:38:26 +0530

    Complete

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express =require("express")
 const app = express();
 const PORT = 4000;
 
-app.get('/',(req,res)=>{
-    res.send("Hello World!")
+app.get('/ping',(req,res)=>{
+    res.send("pong!")
 })
 
 app.listen(PORT,()=>{
