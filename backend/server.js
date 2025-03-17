17847d6ce2014fb47571c57db051ed6e58d20449
suba dharshini <subakarthick2005@gmail.com>
2025-03-17 16:43:55 +0530

    create folder

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config(); 
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const dotenv =require("dotenv");
+const connectDatabase = require('./database');
+const mongoose = require('mongoose');
+
+dotenv.config();
+const Routes = require("./routes");
+app.use(express.json());
+app.use('/api',Routes);
+
+connectDatabase();
+
+app.get('/', (req, res) => {
+    let dbStatus;
+    switch (mongoose.connection.readyState) {
+        case 0:
+            dbStatus = 'Disconnected';
+            break;
+        case 1:
+            dbStatus = 'Connected';
+            break;
+        case 2: 
+            dbStatus = 'Connecting';
+            break;  
+        case 3:
+            dbStatus = 'Disconnecting';
+            break;
+        default:
+            dbStatus = 'Unknown state';
+            break;
+    }
+            res.json({ status: dbStatus });
+        });
+        
+        
+        app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port}`);
+        });
